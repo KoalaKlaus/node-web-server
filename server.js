da7e86c015134b38188988413d0a574ccd6d1a95
Nicolaev Nicolae <nikolaevnick9@gmail.com>
2017-07-13 22:55:09 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${req.ip} : ${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log+'\n');
+    next();
+});
+/* ===MAINTENANCE MODE===
+app.use((req, res, next) => {
+    res.render('maintenance.hbs', {
+        pageTitle: 'Doing things, be patient!',
+        message: 'We are currently under maintenance! We should be back soon!'
+    });
+});
+*/
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Index',
+        welcomeMessage: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt sem sit amet mi maximus.',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Running @ port 3000');
+});
